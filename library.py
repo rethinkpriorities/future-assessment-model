2531f8bdfec44174f39544d1af96fa4286b41d69
Peter Hurford <peter@peterhurford.com>
2022-08-26 14:41:13 -0500

    Sample from arbitrary functions; cleanup

--- library.py
+++ library.py
@@ -5,8 +5,8 @@ from scipy import stats
 
 
 def normal_sample(low, high, interval):
-    if (low > high) or (high < low):
-        raise ValueError
+    if low > high:
+        raise ValueError('`high value` cannot be lower than `low value`')
     if low == high:
         return low
     else:
@@ -18,8 +18,10 @@ def normal_sample(low, high, interval):
 
     
 def lognormal_sample(low, high, interval):
-    if (low > high) or (high < low):
-        raise ValueError
+    if low > high:
+        raise ValueError('`high value` cannot be lower than `low value`')
+    if low < 0:
+        raise ValueError('lognormal_sample cannot handle negative values')
     if low == high:
         return low
     else:
@@ -33,8 +35,8 @@ def lognormal_sample(low, high, interval):
 
 
 def t_sample(low, high, t, interval):
-    if (low > high) or (high < low):
-        raise ValueError
+    if low > high:
+        raise ValueError('`high value` cannot be lower than `low value`')
     if low == high:
         return low
     else:
@@ -44,8 +46,10 @@ def t_sample(low, high, t, interval):
 
 
 def log_t_sample(low, high, t, interval):
-    if (low > high) or (high < low):
-        raise ValueError
+    if low > high:
+        raise ValueError('`high value` cannot be lower than `low value`')
+    if low < 0:
+        raise ValueError('log_t_sample cannot handle negative values')
     if low == high:
         return low
     else:
@@ -56,23 +60,20 @@ def log_t_sample(low, high, t, interval):
         return np.exp(np.random.standard_t(t) * rangex * 0.6/interval + mu)
     
 
-def norm(x, y, lclip=None):
-    return [x, y, 'norm', lclip]
+def norm(x, y, lclip=None, rclip=None):
+    return [x, y, 'norm', lclip, rclip]
 
-def norm_lclip(x, y):
-    return [x, y, 'norm', x]
+def const(x):
+    return [x, None, 'const', None, None]
 
-def lognorm(x, y, lclip=None):
-    return [x, y, 'log', lclip]
+def lognorm(x, y, lclip=None, rclip=None):
+    return [x, y, 'log', lclip, rclip]
 
-def lognorm_lclip(x, y):
-    return [x, y, 'log', x]
+def weighted_lognorm(logs, weights, lclip=None, rclip=None):
+    return [logs, weights, 'weighted_log', lclip, rclip]
 
-def weighted_lognorm(logs, weights):
-    return [logs, weights, 'weighted_log']
-
-def distributed_lognorm(logs, weights):
-    return [logs, weights, 'distributed_log']
+def distributed_lognorm(logs, weights, lclip=None, rclip=None):
+    return [logs, weights, 'distributed_log', lclip, rclip]
 
 def tdist(x, y, t, lclip=None, rclip=None):
     return [x, y, 'tdist', t, lclip, rclip]
@@ -81,75 +82,74 @@ def log_tdist(x, y, t, lclip=None, rclip=None):
     return [x, y, 'log-tdist', t, lclip, rclip]
 
 
-def sample(var, allow_negative=False, credibility=0.9):
-    if len(var) > 2:
-        if var[2] == 'norm':
-            out = normal_sample(var[0], var[1], credibility)
-            lclip = var[3]
-            if lclip and out < lclip:
-                out = lclip
+def sample(var, credibility=0.9):
+    if callable(var):
+        return var()
 
-        elif var[2] == 'log':
-            out = lognormal_sample(var[0], var[1], credibility)
-            lclip = var[3]
-            if lclip and out < lclip:
-                out = lclip
+    if not isinstance(var, list) and not (len(var) == 5 or len(var) == 6):
+        raise ValueError('input to sample is malformed - must be sample data')
 
-        elif var[2] == 'weighted_log':
-            weights = var[1]
-            sum_weights = sum(weights)
-            if sum_weights <= 0.99 or sum_weights >= 1.01:
-                raise ValueError('weighted_log weights don\'t sum to 1 - they sum to {}'.format(sum_weights))
-            if len(weights) != len(var[0]):
-                raise ValueError('weighted_log weights and distributions not same length')
-            log_result = 0
-            for i, log_data in enumerate(var[0]):
-                log_value = lognormal_sample(log_data[0], log_data[1], credibility)
-                weight = weights[i]
-                log_result += log_value * weight
-            out = log_result
+    if var[2] == 'const':
+        out = var[0]
 
-        elif var[2] == 'distributed_log':
-            weights = var[1]
-            sum_weights = sum(weights)
-            if sum_weights <= 0.99 or sum_weights >= 1.01:
-                raise ValueError('distributed_log weights don\'t sum to 1 - they sum to {}'.format(sum_weights))
-            if len(weights) != len(var[0]):
-                raise ValueError('distributed_log weights and distributions not same length')
-            r_ = random.random()
-            weights = np.cumsum(weights)
-            done = False
-            for i, log_data in enumerate(var[0]):
-                if not done:
-                    weight = weights[i]
-                    if r_ <= weight:
-                        out = lognormal_sample(log_data[0], log_data[1], credibility)
-                        done = True
-
-        elif var[2] == 'tdist':
-            out = t_sample(var[0], var[1], var[3], credibility)
-            lclip = var[4]
-            rclip = var[5]
-            if lclip and out < lclip:
-                out = lclip
-            if rclip and out > rclip:
-                out = rclip
-
-        elif var[2] == 'log-tdist':
-            out = log_t_sample(var[0], var[1], var[3], credibility)
-            lclip = var[4]
-            rclip = var[5]
-            if lclip and out < lclip:
-                out = lclip
-            if rclip and out > rclip:
-                out = rclip
-
-        else:
-            raise ValueError
-    else:
+    elif var[2] == 'norm':
         out = normal_sample(var[0], var[1], credibility)
-    if out < 0 and not allow_negative:
-        out = 0
+
+    elif var[2] == 'log':
+        out = lognormal_sample(var[0], var[1], credibility)
+
+    elif var[2] == 'weighted_log':
+        weights = var[1]
+        sum_weights = sum(weights)
+        if sum_weights <= 0.99 or sum_weights >= 1.01:
+            raise ValueError('weighted_log weights don\'t sum to 1 - they sum to {}'.format(sum_weights))
+        if len(weights) != len(var[0]):
+            raise ValueError('weighted_log weights and distributions not same length')
+        log_result = 0
+        for i, log_data in enumerate(var[0]):
+            log_value = lognormal_sample(log_data[0], log_data[1], credibility)
+            weight = weights[i]
+            log_result += log_value * weight
+        out = log_result
+
+    elif var[2] == 'distributed_log':
+        weights = var[1]
+        sum_weights = sum(weights)
+        if sum_weights <= 0.99 or sum_weights >= 1.01:
+            raise ValueError('distributed_log weights don\'t sum to 1 - they sum to {}'.format(sum_weights))
+        if len(weights) != len(var[0]):
+            raise ValueError('distributed_log weights and distributions not same length')
+        r_ = random.random()
+        weights = np.cumsum(weights)
+        done = False
+        for i, log_data in enumerate(var[0]):
+            if not done:
+                weight = weights[i]
+                if r_ <= weight:
+                    out = lognormal_sample(log_data[0], log_data[1], credibility)
+                    done = True
+
+    elif var[2] == 'tdist':
+        out = t_sample(var[0], var[1], var[3], credibility)
+
+    elif var[2] == 'log-tdist':
+        out = log_t_sample(var[0], var[1], var[3], credibility)
+
+    else:
+        raise ValueError('{} sampler not found'.format(var[2]))
+
+    if var[2] == 'tdist' or var[2] == 'log-tdist':
+        lclip = var[4]
+        rclip = var[5]
+    else:
+        lclip = var[3]
+        rclip = var[4]
+
+    if lclip and out < lclip:
+        out = lclip
+    if rclip and out > rclip:
+        out = rclip
+
     return out
 
 
