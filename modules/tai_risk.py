f10a521d0a3c791865ad66e65b211748aadcb4bb
Peter Hurford <peter@peterhurford.com>
2023-03-23 19:19:50 -0400

    deployment coordination

--- modules/tai_risk.py
+++ modules/tai_risk.py
@@ -8,7 +8,7 @@ def tai_scenarios_module(y, state, verbose):
             if sq.event(p_tai_intentional_misuse(state['war'])):
                 # TODO: Parameterize the 0.5 (Who gets to TAI first affecting risk)
                 # TODO: May depend on who the war is between?
-                if 0.5 * sq.event(p_alignment_solved(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose)):
+                if 0.5 * sq.event(p_alignment_solved(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose) * p_alignment_deployment_safety_and_coordination(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose)):
                     if verbose:
                         # TODO: aligned by default? subtle misalignment?
                         # TODO: announce intentional misuse
@@ -42,7 +42,7 @@ def tai_scenarios_module(y, state, verbose):
                             print('...Singleton is catastrophic')
                         state['catastrophe'].append(state['category'])
                     state['terminate'] = True; state['final_year'] = y
-            elif sq.event(p_alignment_solved(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose)):
+            elif sq.event(p_alignment_solved(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose) * p_alignment_deployment_safety_and_coordination(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose)):
                 if sq.event(p_subtle_alignment_solved):
                     if verbose:
                         print('{}: ...Achieved aligned TAI (aligned via work, {} attempt), happy future! :D'.format(y, '2nd+' if state['tai'] else 'first'))
@@ -70,40 +70,44 @@ def tai_scenarios_module(y, state, verbose):
                     state['category'] = 'xrisk_subtly_unaligned_tai'
                     state['tai_type'] = 'agent'
                     state['terminate'] = True; state['final_year'] = y
-            elif sq.event(p_full_tai_misalignment_averted):
-                state['averted_misalignment'] = True
-                if sq.event(p_tai_misalignment_averting_is_catastrophic):
-                    # TODO: Right now this guarantees abandonment if catastrophe - revise?
+            # Coordinating not to deploy unsafe AI
+            elif not p_alignment_deployment_safety_and_coordination(state['war'], y - CURRENT_YEAR, first_attempt=not state['tai'], verbose=verbose):
+                if sq.event(p_full_tai_misalignment_averted):
+                    state['averted_misalignment'] = True
+                    if sq.event(p_tai_misalignment_averting_is_catastrophic):
+                        # TODO: Right now this guarantees abandonment if catastrophe - revise?
+                        if verbose:
+                            print('{}: ...Misaligned TAI happened, it was averted with catastrophe, and we abandon TAI'.format(y))
+                        state['tai_type'] = 'abandoned'
+                        state['catastrophe'].append('averting_misaligned_tai')
+                        # TODO: Maybe resume TAI with lower chance of happening?
+                    elif sq.event(p_full_tai_misalignment_averted_means_abandoned_tai):
+                        if verbose:
+                            print('{}: ...Misaligned TAI happened, it was averted with no catastrophe, and we abandon TAI'.format(y))
+                        state['tai_type'] = 'abandoned'    
+                        # TODO: Maybe resume TAI with lower chance of happening?
+                    elif verbose:
+                        print('{}: ...Misaligned TAI happened, but it was averted'.format(y))
+                        state['tai_type'] = 'agent'
+                elif sq.event(p_tai_xrisk_is_extinction):
                     if verbose:
-                        print('{}: ...Misaligned TAI happened, it was averted with catastrophe, and we abandon TAI'.format(y))
-                    state['tai_type'] = 'abandoned'
-                    state['catastrophe'].append('averting_misaligned_tai')
-                    # TODO: Maybe resume TAI with lower chance of happening?
-                elif sq.event(p_full_tai_misalignment_averted_means_abandoned_tai):
-                    if verbose:
-                        print('{}: ...Misaligned TAI happened, it was averted with no catastrophe, and we abandon TAI'.format(y))
-                    state['tai_type'] = 'abandoned'    
-                    # TODO: Maybe resume TAI with lower chance of happening?
-                elif verbose:
-                    print('{}: ...Misaligned TAI happened, but it was averted'.format(y))
+                        print('{}: ...XRISK from fully unaligned TAI (extinction) :('.format(y))
+                    state['category'] = 'xrisk_full_unaligned_tai_extinction'
                     state['tai_type'] = 'agent'
-            elif sq.event(p_tai_xrisk_is_extinction):
-                if verbose:
-                    print('{}: ...XRISK from fully unaligned TAI (extinction) :('.format(y))
-                state['category'] = 'xrisk_full_unaligned_tai_extinction'
-                state['tai_type'] = 'agent'
-                state['catastrophe'].append(state['category'])
-                state['terminate'] = True; state['final_year'] = y
-            else:
-                if verbose:
-                    print('{}: ...XRISK from fully unaligned TAI (singleton) :('.format(y))
-                state['category'] = 'xrisk_full_unaligned_tai_singleton'
-                state['tai_type'] = 'agent'
-                if sq.event(p_tai_singleton_is_catastrophic):
-                    if verbose:
-                        print('...Singleton is catastrophic')
                     state['catastrophe'].append(state['category'])
-                state['terminate'] = True; state['final_year'] = y
+                    state['terminate'] = True; state['final_year'] = y
+                else:
+                    if verbose:
+                        print('{}: ...XRISK from fully unaligned TAI (singleton) :('.format(y))
+                    state['category'] = 'xrisk_full_unaligned_tai_singleton'
+                    state['tai_type'] = 'agent'
+                    if sq.event(p_tai_singleton_is_catastrophic):
+                        if verbose:
+                            print('...Singleton is catastrophic')
+                        state['catastrophe'].append(state['category'])
+                    state['terminate'] = True; state['final_year'] = y
+            else:
+                pass # Don't deploy unsafe AI, retry next year
         else: # tool TAI
             if verbose:
                 print('{}: ...Tool TAI made'.format(y))
