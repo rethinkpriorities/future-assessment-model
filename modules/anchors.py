4ac66af48855f47d0196d050e347db29ffed241e
Peter Wildeford <peter@peterhuford.com>
2023-06-17 16:12:13 -0400

    some initial cleaning and writing on anchors

--- modules/anchors.py
+++ modules/anchors.py
@@ -1,3 +1,9 @@
+import squigglepy as sq
+import numpy as np
+
+from pprint import pprint
+
+
 def get_anchor_gpt_benchmark_pct(anchor, target):
     out = round(np.mean([a <= target for a in anchor]) * 100, 1)
     return '<0.1%' if out == 0 else str(out) + '%'
@@ -9,11 +15,10 @@ def compare_anchor_to_gpt(anchor):
     print('GPT3 (~23 log FLOP) can do it: {}'.format(get_anchor_gpt_benchmark_pct(anchor, 23)))
     print('GPT4 (~25 log FLOP) can do it: {}'.format(get_anchor_gpt_benchmark_pct(anchor, 25)))
     print('GPT5 (~27 log FLOP) can do it: {}'.format(get_anchor_gpt_benchmark_pct(anchor, 27)))
-    print('GPT6 (~28 log FLOP) can do it: {}'.format(get_anchor_gpt_benchmark_pct(anchor, 28)))
 
 
-def tai_log_flop_needs(brain, efficiency, transformative_vs_human, horizon_length, scaling_exponent,
-                       flops_per_param_per_sec, params=None, ref_params=11.2, ref_params_samples=12,
+def tai_log_flop_needs(brain, efficiency=0, transformative_vs_human=0, horizon_length=0, scaling_exponent=0,
+                       flops_per_param_per_sec=0, params=None, ref_params=11.2, ref_params_samples=12,
                        bayes_update=None):
     params_ = brain + efficiency - flops_per_param_per_sec if params is None else params
     dist = ((brain + efficiency + transformative_vs_human + horizon_length + ref_params_samples) -
@@ -27,6 +32,7 @@ def tai_log_flop_needs(brain, efficiency, transformative_vs_human, horizon_lengt
 
 @np.vectorize
 def cotra_bayes_update_against_low_flop(f):
+    import random
     f = f + ~sq.norm(1,3) if f < 27 and random.random() > 0.3 else f
     f = f + ~sq.norm(1,3) if f < 26 and random.random() > 0.2 else f
     f = f + ~sq.norm(1,3) if f < 25 and random.random() > 0.1 else f
@@ -55,79 +61,150 @@ def cotra_anchor(horizon_length, bayes_update=cotra_bayes_update_against_low_flo
                               bayes_update=bayes_update)
 
 
-def plot_anchors(anchor1=None, anchor2=None, anchor3=None, bins=100, alpha=0.6, label1=None, label2=None, label3=None,
-                verbose=True, xlim=[20, 75], figsize=(10,8)):
+def plot_anchors(anchor1=None, anchor2=None, anchor3=None, anchor4=None, anchor5=None, anchor6=None,
+                 label1=None, label2=None, label3=None, label4=None, label5=None, label6=None,
+                 bins=100, alpha=0.6, verbose=True, xlim=[20, 75], figsize=(10,8), disable_lines=False):
     if label1 is None:
         label1 = 'Anchor1'
     if label2 is None:
         label2 = 'Anchor2'
     if label3 is None:
         label3 = 'Anchor3'
+    if label4 is None:
+        label4 = 'Anchor4'
+    if label5 is None:
+        label5 = 'Anchor5'
+    if label6 is None:
+        label6 = 'Anchor6'
         
     if anchor1 is None:
-        raise ValueError
+        raise ValueError('Must define {}'.format(label1))
         
     if anchor2 is None and anchor3 is not None:
         raise ValueError('{} defined without defining {}'.foramt(label3, label2))
 
-    if anchor2 is not None and len(anchor1) != len(anchor2):
+    if anchor3 is None and anchor4 is not None:
+        raise ValueError('{} defined without defining {}'.foramt(label4, label3))
+
+    if anchor4 is None and anchor5 is not None:
+        raise ValueError('{} defined without defining {}'.foramt(label5, label4))
+
+    if anchor2 is not None and isinstance(anchor2, np.ndarray) and len(anchor1) != len(anchor2):
         raise ValueError('{} and {} do not match length'.format(label1, label2))
 
-    if anchor3 is not None and len(anchor2) != len(anchor3):
+    if anchor3 is not None and isinstance(anchor3, np.ndarray) and len(anchor2) != len(anchor3):
         raise ValueError('{} and {} do not match length'.format(label2, label3))
+
+    if anchor4 is not None and isinstance(anchor4, np.ndarray) and len(anchor3) != len(anchor4):
+        raise ValueError('{} and {} do not match length'.format(label3, label4))
+
+    if anchor5 is not None and isinstance(anchor5, np.ndarray) and len(anchor4) != len(anchor5):
+        raise ValueError('{} and {} do not match length'.format(label4, label5))
+
+    if anchor6 is not None and isinstance(anchor6, np.ndarray) and len(anchor5) != len(anchor6):
+        raise ValueError('{} and {} do not match length'.format(label5, label6))
         
     if verbose:
         print(label1)
         pprint(sq.get_percentiles(anchor1, digits=1))
         compare_anchor_to_gpt(anchor1)
         print('-')
-        if anchor2 is not None:
+        if anchor2 is not None and isinstance(anchor2, np.ndarray):
             print(label2)
             pprint(sq.get_percentiles(anchor2, digits=1))
             compare_anchor_to_gpt(anchor2)
             print('-')
-        if anchor3 is not None:
+        if anchor3 is not None and isinstance(anchor3, np.ndarray):
             print(label3)
             pprint(sq.get_percentiles(anchor3, digits=1))
             compare_anchor_to_gpt(anchor3)
             print('-')
+        if anchor4 is not None and isinstance(anchor4, np.ndarray):
+            print(label4)
+            pprint(sq.get_percentiles(anchor4, digits=1))
+            compare_anchor_to_gpt(anchor4)
+            print('-')
+        if anchor5 is not None and isinstance(anchor5, np.ndarray):
+            print(label5)
+            pprint(sq.get_percentiles(anchor5, digits=1))
+            compare_anchor_to_gpt(anchor5)
+            print('-')
+        if anchor6 is not None and isinstance(anchor6, np.ndarray):
+            print(label6)
+            pprint(sq.get_percentiles(anchor6, digits=1))
+            compare_anchor_to_gpt(anchor6)
+            print('-')
         
-    if xlim:
-        anchor1 = [xlim[0] if a < xlim[0] else a for a in anchor1]
-        anchor1 = [xlim[1] if a > xlim[1] else a for a in anchor1]
-        if anchor2 is not None:
-            anchor2 = [xlim[0] if a < xlim[0] else a for a in anchor2]
-            anchor2 = [xlim[1] if a > xlim[1] else a for a in anchor2]
-        if anchor3 is not None:
-            anchor3 = [xlim[0] if a < xlim[0] else a for a in anchor3]
-            anchor3 = [xlim[1] if a > xlim[1] else a for a in anchor3]
         
+    import matplotlib
+    import matplotlib.pyplot as plt
     plt.figure(figsize=figsize)
+
     plt.hist(anchor1, bins=bins, alpha=alpha, label=label1, color='black', lw=0)
+
     if anchor2 is not None:
-        plt.hist(anchor2, bins=bins, alpha=alpha, label=label2, color='limegreen', lw=0)
+        if isinstance(anchor2, np.ndarray):
+            plt.hist(anchor2, bins=bins, alpha=alpha, label=label2, color='limegreen', lw=0)
+        else:
+            plt.axvline(anchor2, label=label2, color='limegreen')
+
     if anchor3 is not None:
-        plt.hist(anchor3, bins=bins, alpha=alpha, label=label3, color='red', lw=0)
+        if isinstance(anchor3, np.ndarray):
+            plt.hist(anchor3, bins=bins, alpha=alpha, label=label3, color='red', lw=0)
+        else:
+            plt.axvline(anchor3, label=label3, color='red')
+
+    if anchor4 is not None:
+        if isinstance(anchor4, np.ndarray):
+            plt.hist(anchor4, bins=bins, alpha=alpha, label=label4, color='blue', lw=0)
+        else:
+            plt.axvline(anchor4, label=label4, color='blue')
+
+    if anchor5 is not None:
+        if isinstance(anchor5, np.ndarray):
+            plt.hist(anchor5, bins=bins, alpha=alpha, label=label5, color='orange', lw=0)
+        else:
+            plt.axvline(anchor5, label=label5, color='orange')
+
+    if anchor6 is not None:
+        if isinstance(anchor6, np.ndarray):
+            plt.hist(anchor6, bins=bins, alpha=alpha, label=label6, color='brown', lw=0)
+        else:
+            plt.axvline(anchor6, label=label6, color='brown')
         
-    plt.axvline(np.mean(anchor1), label='{} (mean)'.format(label1), color='black')
-    if anchor2 is not None:
-        plt.axvline(np.mean(anchor2), label='{} (mean)'.format(label2), color='green')
-    if anchor3 is not None:
-        plt.axvline(np.mean(anchor3), label='{} (mean)'.format(label2), color='red')
+    if not disable_lines:
+        plt.axvline(np.mean(anchor1), label='{} (mean)'.format(label1), color='black')
+        plt.axvline(np.percentile(anchor1, q=10), label='{} (10% CI)'.format(label1), color='black', linestyle='--')
+        plt.axvline(np.percentile(anchor1, q=90), label='{} (90% CI)'.format(label1), color='black', linestyle='--')
+
+        if anchor2 is not None and isinstance(anchor2, np.ndarray):
+            plt.axvline(np.mean(anchor2), label='{} (mean)'.format(label2), color='green')
+            plt.axvline(np.percentile(anchor2, q=10), label='{} (10% CI)'.format(label2), color='green', linestyle='--')
+            plt.axvline(np.percentile(anchor2, q=90), label='{} (90% CI)'.format(label2), color='green', linestyle='--')
+
+        if anchor3 is not None and isinstance(anchor3, np.ndarray):
+            plt.axvline(np.mean(anchor3), label='{} (mean)'.format(label2), color='red')
+            plt.axvline(np.percentile(anchor3, q=10), label='{} (10% CI)'.format(label3), color='red', linestyle='--')
+            plt.axvline(np.percentile(anchor3, q=90), label='{} (90% CI)'.format(label3), color='red', linestyle='--')
+
+        if anchor4 is not None and isinstance(anchor4, np.ndarray):
+            plt.axvline(np.mean(anchor4), label='{} (mean)'.format(label2), color='blue')
+            plt.axvline(np.percentile(anchor4, q=10), label='{} (10% CI)'.format(label4), color='blue', linestyle='--')
+            plt.axvline(np.percentile(anchor4, q=90), label='{} (90% CI)'.format(label4), color='blue', linestyle='--')
+
+        if anchor5 is not None and isinstance(anchor5, np.ndarray):
+            plt.axvline(np.mean(anchor5), label='{} (mean)'.format(label2), color='orange')
+            plt.axvline(np.percentile(anchor5, q=10), label='{} (10% CI)'.format(label5), color='orange', linestyle='--')
+            plt.axvline(np.percentile(anchor5, q=90), label='{} (90% CI)'.format(label5), color='orange', linestyle='--')
+
+        if anchor6 is not None and isinstance(anchor6, np.ndarray):
+            plt.axvline(np.mean(anchor6), label='{} (mean)'.format(label2), color='brown')
+            plt.axvline(np.percentile(anchor6, q=10), label='{} (10% CI)'.format(label6), color='brown', linestyle='--')
+            plt.axvline(np.percentile(anchor6, q=90), label='{} (90% CI)'.format(label6), color='brown', linestyle='--')
     
-    plt.axvline(np.percentile(anchor1, q=10), label='{} (10% CI)'.format(label1), color='black', linestyle='--')
-    if anchor2 is not None:
-        plt.axvline(np.percentile(anchor2, q=10), label='{} (10% CI)'.format(label2), color='green', linestyle='--')
-    if anchor3 is not None:
-        plt.axvline(np.percentile(anchor3, q=10), label='{} (10% CI)'.format(label3), color='red', linestyle='--')
-    
-    plt.axvline(np.percentile(anchor1, q=90), label='{} (90% CI)'.format(label1), color='black', linestyle='--')
-    if anchor2 is not None:
-        plt.axvline(np.percentile(anchor2, q=90), label='{} (90% CI)'.format(label2), color='green', linestyle='--')
-    if anchor3 is not None:
-        plt.axvline(np.percentile(anchor3, q=90), label='{} (90% CI)'.format(label3), color='red', linestyle='--')
-        
     plt.xlim(xlim)
+    plt.xlabel('log10 of total FLOP for training')
+    plt.title('Distribution of total training compute needed to meet human-level performance')
     plt.legend()
     plt.show()
     return None
